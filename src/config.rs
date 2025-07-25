f65cf949e9c262648ed6c4fe9baa50ae01fccaec
mwmyway <king_djh@163.com>
2025-07-25 23:07:44 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.wefun.eu.org"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.010121.xyz"];
 pub const RS_PUB_KEY: &str = "rd123456";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
